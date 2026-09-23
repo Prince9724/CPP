c81989247444f7c16c88b2be96fac45fce71fe7f
Prince gond <princegondrw123@gmail.com>
2026-09-23 20:17:20 +0530

    ../,

--- fullstack/project-1/backend/controllers/authController.js
+++ fullstack/project-1/backend/controllers/authController.js
@@ -1,5 +1,42 @@
 import user from "../models/authModel.js"
+import bcrypt from "bcrypt"
+export const signUp= async(req , res)=>{
+   try{
+      const password = req.body.password ; 
+    const hash = bcrypt.hash(password , 10);
 
-const AddUser= async(req , res)=>{
-    
+     const result = await user.create({...req.body,password :hash});
+    res.json({
+        status:true,
+        message:"user signUp succesfully !! ",
+        data :result
+    })
+   }
+   catch(err){
+     res.json({
+        status:false , 
+        message:" user signUp failed !! ",
+        err :err.message
+     })
+   }
+}
+
+
+export const signIn = async (req, res )=>{
+
+    try{
+        const {email, password} = req.body
+        const userData =  await user.find({email});
+        if(!userData){
+            return res.json({
+                status:false ,
+                message :"user not found !! "
+            })
+        }
+        const isMatch = await bcrypt.compare(password , userData.password);
+
+    }
+    catch(err){
+
+    }
 }
