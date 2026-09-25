9ec06d387fc9af6fd6b48738d3b6796b1ffb01c7
Prince gond <princegondrw123@gmail.com>
2026-09-25 08:08:10 +0530

    ././

--- fullstack/project-2/controller/authController.js
+++ fullstack/project-2/controller/authController.js
@@ -6,14 +6,44 @@ export const signUp = async(req, res)=>{
         try{
             const {email, name , password}= req.body ;  
             const hash = bcrypt.hash(password , 12);
+            const result = await user.create();
+            res.json({
+               status:true,
+               message:"signup succesfully !! ",
+               result 
+            })
 
-            
         }
         catch(err){
             res.json({
             status:false,
-            message:"mongodb connection failled !! ",
+            message:"signup  failled !! ",
             err : err.message
         })
         }
+}
+export const signIn = async (req , res )=>{
+    try{
+        const {email, password , name } =  req.body ; 
+
+        const result = await user.find({email});
+        if(!result){
+            res.json({
+                status:false,
+                message:"user not found !! ",
+                err :err.message
+            })
+        }
+        const isMatch =  await bcrypt.compare(password , user,password);
+
+    }
+    catch(err){
+        res.json({
+            status:false,
+            message:"signIn  failled !! ",
+            err : err.message
+        })
+    }
+
+
 }
